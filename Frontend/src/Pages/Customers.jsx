990c7dc589fa22ed972fa18c5fc89ea1ed318c5e
walidaitbaha <walidbaha17@gmail.com>
2025-02-26 21:24:12 +0000

    Customer front-end

--- Frontend/src/Pages/Customers.jsx
+++ Frontend/src/Pages/Customers.jsx
@@ -85,7 +85,7 @@ export default function Customers() {
 
   return (
    
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto h-screen">
         <div className="flex items-center justify-between mb-8">
           <h1 className={`text-3xl font-bold ${darkMode ? 'text-white' : 'text-gray-800'}`}>
             Customers
